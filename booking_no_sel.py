b90794fe3f177882b9a8003f8e39df5d02a1062d
Vasiloudis <vasiloudis5@gmail.com>
2021-12-02 12:52:27 +0200

    Scraper Fix

--- booking_no_sel.py
+++ booking_no_sel.py
@@ -5,9 +5,9 @@ import pymongo
 from pymongo import MongoClient
 import numpy as np
 import re
+from datetime import date
 # from requests.exceptions import ConnectionError
 # import time
-import socket
 
 
 links = []
@@ -15,6 +15,7 @@ i = 0
 headers = []
 facilities = []
 scores = []
+choices = []
 rooms = []
 sleeps = []
 hotel = []
@@ -26,39 +27,46 @@ record1 = { "appliance":"fan",
 cluster = MongoClient("mongodb+srv://Vasiloudis:Vasiloudis@myCluster.bjuk6.mongodb.net/booking?ssl=true&ssl_cert_reqs=CERT_NONE")
 my_db = cluster["booking"]
 my_collection = my_db["hotels"]
+basic_data = my_db["basic_data"]
 # test = {"id": 0, "name": "a Name", "age": 25}
 # my_collection.insert_one(test)
 
 
 #my_collection.drop() #gia delete collection
 
-
 in_month = 1
-in_day = 25
+in_day = 20
 in_year = 2022
 out_month = 1
 out_day = 30
 out_year = 2022
-people = 2
+people = 1
 city = "Skiathos"
 country = "Greece"
 
+f_date = date(in_year, in_month, in_day)
+l_date = date(out_year, out_month, out_day)
+total_days = l_date - f_date
+#print(total_days.days)
 
+#my_db.basic_data.insert_one({"in_year" : in_year ,"out_year" : out_year, "in_month" : in_month, "out_month" : out_month, "in_day" : in_day, "out_day" : out_day , "people" : people, "city" : city, "country" : country})
+
+#exit() 
 link = "https://www.booking.com/searchresults.html?checkin_month={in_month}&checkin_monthday={in_day}" \
     "&checkin_year={in_year}&checkout_month={out_month}&checkout_monthday={out_day}&checkout_year={out_year}" \
     "&group_adults={people}&group_children=0&order=price&ss={city}%2C%20{country}" \
-        .format(in_month=in_month,
-                in_day=in_day,
-                in_year=in_year,
-                out_month=out_month,
-                out_day=out_day,
-                out_year=out_year,
-                people=people,
-                city=city,
-                country=country)
+        .format(in_month=my_db.basic_data.find_one({"in_month" : in_month})['in_month'],
+                in_day=my_db.basic_data.find_one({"in_day" : in_day})['in_day'],
+                in_year=my_db.basic_data.find_one({"in_year" : in_year})['in_year'],
+                out_month=my_db.basic_data.find_one({"out_month" : out_month})['out_month'],
+                out_day=my_db.basic_data.find_one({"out_day" : out_day})['out_day'],
+                out_year=my_db.basic_data.find_one({"out_year" : out_year})['out_year'],
+                people=my_db.basic_data.find_one({"people" : people})['people'],
+                city=my_db.basic_data.find_one({"city" : city})['city'],
+                country=my_db.basic_data.find_one({"country" : country})['country'])
 
 print(link)
-
+#exit()
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.101 Safari/537.36"} # Windows 10 with Google Chrome
 
@@ -88,10 +96,11 @@ for page in pages:
     #for a in soup.find_all('a', {'class':'hotel_name_link'}):
     for a in soup.find_all('a', {'class':'fb01724e5b'}):
         links.insert(0, a['href'].strip())
-    links.reverse()
+    #links.reverse()
     #print(links)
 #print(links)
 print(str(len(links))+" urls found")
+#print(links[0])
 
 while i < len(links):
     # page = requests.get('https://booking.com'+links[i], headers = headers)
@@ -116,18 +125,23 @@ while i < len(links):
     #print("")
 
     hotel_id = soup.find('p', class_='hp-lists-counter').attrs['data-hotel-id'].strip()
-    #print("Hotel ID :"+ hotel_id)
+    print("Hotel ID :"+ hotel_id)
     img_link = soup.find('img', class_='hide').attrs['src']
     #print("Image link :"+img_link)
     hotel_link = links[i]
-    #print("Hotel link :"+ hotel_link)
-    name = soup.find('h2', class_='hp__hotel-name').text.strip()
+    print("Hotel link :"+ hotel_link)
+    #name = soup.find('h2', class_='hp__hotel-name').text.strip()
+    #print(name)
+    name_temp = soup.find('h2', class_='hp__hotel-name')
+    type = name_temp.find('span', class_="bui-badge").text.strip() if name_temp.find('span', class_="bui-badge") else  name_temp.find('span', class_="hp__hotel-type-badge").text.strip()
+    #print(type)
+    name = str(name_temp).split("</span>")[1].split("<")[0].strip()
     #print(name)
     address = soup.find('span', class_='hp_address_subtitle').text.strip()
     #print(address)
-    reviews = soup.find('div', class_='_6a1b6ff88e').text.strip() if soup.find('div', class_='_6a1b6ff88e') else "0 reviews"
+    reviews = soup.find('div', class_='_6a1b6ff88e').text.strip() if soup.find('div', class_='_6a1b6ff88e') else -1
     #print(reviews)
-    rating = soup.find('div', class_='e5a32fd86b').text.strip() if soup.find('div', class_='e5a32fd86b') else "None"
+    rating = soup.find('div', class_='e5a32fd86b').text.strip() if soup.find('div', class_='e5a32fd86b') else -1
     #print(rating)
     #print("")
 
@@ -159,13 +173,33 @@ while i < len(links):
 
 
 
-        sleep = row.find('span', class_="bui-u-sr-only").text.strip()
-        #print("Sleeps :",sleep)
-        price = row.find('span', class_="prco-valign-middle-helper").text.strip()
+       
+        sleep_temp = row.find('span', class_="bui-u-sr-only").text.strip()
+        if ('-' in sleep_temp):  
+            sleep = sleep_temp.split('-')[1].split('g')[0].strip()
+            #print(sleep) 
+        else:
+            sleep = sleep_temp.split(':')[1].strip()
+            #print(sleep)
+                   
+
+
+
+        #print("Sleeps :",int(sleep))
+        
+        price_temp = row.find('span', class_="prco-valign-middle-helper").text.strip()
+        price = price_temp.split()[1]
+        if ',' in price:
+            price = price.replace(",", "")
         #print("Price :",price)
-        #choices_temp = row.find('ul', class_="hprt-conditions-bui").text if soup.find('ul', class_="hprt-conditions-bui") else "no Choices"
-        choices_temp = row.find('td', class_="hprt-table-cell-conditions").text
-        choices = " ".join(choices_temp.split())
+        # choices_temp = row.find('td', class_="hprt-table-cell-conditions").text
+        # choices = " ".join(choices_temp.split())
+        ul2 = row.find('td', class_="hprt-table-cell-conditions") #'ul', class_="hprt-conditions-bui" #error
+        choices = []
+        for li2 in ul2.find_all('li', class_="bui-list__item"):
+            choice = li2.find('div', class_="bui-list__description").text.strip()
+            choices.append(choice)
+       
         #print(choices)
         #print("Choices :",choices)
         select = row.find('select', class_="hprt-nos-select")
@@ -175,11 +209,11 @@ while i < len(links):
             options.append(option.text.strip().replace("\n\xa0\n\xa0\xa0\xa0\n"," ").replace("\xa0",""))
         price_per_room = options[1:]
         #print("Price per Room :", price_per_room)
-        sleeps = [ {"persons": sleep, "price": price, "choices": choices," price_per_room": price_per_room} ]
+        sleeps = [ {"max_persons": int(sleep), "price": int(price), "choices": choices," price_per_room": price_per_room} ]
 
         rooms += [ {"id": roomId, "type": roomType,"facilities": facilities, "sleeps": sleeps}]
         facilities = []
-
+        
         #print(sleeps)
         # for facility_temp in row.find_all('div', class_="hprt-facilities-facility"):
         #     facilities.append(facility_temp.span.text.strip())
@@ -187,7 +221,7 @@ while i < len(links):
         #     facilities.append(facility.text.strip())
         # print(facilities)
         # facilities = []
-
+    
 
     #print("----------------------------------------------------------------------------------------")
 
@@ -196,15 +230,16 @@ while i < len(links):
 
     #sleeps = [ {"persons": sleep, "price": price, "choices": choices,"facilities": facilities," price_per_room": price_per_room} ]
     #rooms = [ {"id": roomId, "type": roomType, "sleeps": sleeps}]
-    hotel = {"id":hotel_id, "link": hotel_link, "icon": img_link, "name": name, "address": address, "reviews": reviews, "rating": rating, "rooms": rooms}
+    hotel = {"days": int(str(total_days).split(' ')[0]),"link": hotel_link, "icon": img_link, "name": name, "type": type, "address": address, "reviews": reviews, "rating": rating, "rooms": rooms}
     #print(hotel)
     #my_collection.update_one({"check_in": checkin,"check_out": checkout},{ "$set": { "scores": scores, "hotel": hotel} }, True)
     #my_collection.insert({"check_in": checkin,"check_out": checkout,"scores": scores, "hotel": hotel}) #insert doulevei
 
 
-    my_collection.update_one({'key':hotel_id},{"$set": {"check_in": checkin,"check_out": checkout,"scores": scores, "hotel": hotel}}, True)
+    my_collection.update_one({'hotel_id':int(hotel_id)},{"$set": {"scores": scores, "hotel": hotel}}, True)
     rooms = []
     sleeps = []
     scores = []
     facilities = []
+    
     i = i+1
